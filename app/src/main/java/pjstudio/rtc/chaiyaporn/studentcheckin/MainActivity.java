39db293d1f160151d3f67a62acf76774ed8207be
pop <chaip2538@gmail.com>
2015-12-21 16:25:20 +0700

    ฐานข้อมูล

--- app/src/main/java/pjstudio/rtc/chaiyaporn/studentcheckin/MainActivity.java
+++ app/src/main/java/pjstudio/rtc/chaiyaporn/studentcheckin/MainActivity.java
@@ -1,7 +1,16 @@
 package pjstudio.rtc.chaiyaporn.studentcheckin;
 
+import android.os.Build;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +26,36 @@ public class MainActivity extends AppCompatActivity {
         objListTABLE = new ListTABLE(this);
         //Tester
         testAddValue();
+        //synJsonToSQLite
+        SynJSonToSQLite();
 
     }//onCreate
+
+    private void SynJSonToSQLite() {
+
+        //setup Policy
+        if (Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(myPolicy);
+        }//if
+        InputStream objInputStream = null;
+        String strJSon = "";
+
+        //create objInputStream
+        try {
+
+            HttpClient objHttpClient = new DefaultHttpClient();
+            HttpPost objHttpPost = new HttpPost();
+
+        } catch (Exception e) {
+
+            Log.d("Student CheckIn","Error from InputStream ==> " + e.toString());
+
+        }
+
+
+
+    }//synJSonToSQLite
 
     private void testAddValue() {
         objUserTABLE.addValueToUser("User", "Password", "Teacher");
