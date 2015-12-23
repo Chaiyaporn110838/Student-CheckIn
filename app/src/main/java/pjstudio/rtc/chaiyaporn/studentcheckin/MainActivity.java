029d60f1a604050656cda1867e5d1489f58b6359
pop <chaip2538@gmail.com>
2015-12-23 12:39:44 +0700

    ฐานข้อมูลไม่สามารถรับค่าได้

--- app/src/main/java/pjstudio/rtc/chaiyaporn/studentcheckin/MainActivity.java
+++ app/src/main/java/pjstudio/rtc/chaiyaporn/studentcheckin/MainActivity.java
@@ -6,11 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,15 +51,47 @@ public class MainActivity extends AppCompatActivity {
         try {
 
             HttpClient objHttpClient = new DefaultHttpClient();
-            HttpPost objHttpPost = new HttpPost();
-
+            HttpPost objHttpPost = new HttpPost("http://pjstudio.pe.hu/stude/php_get_data%20.php");
+            HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+            HttpEntity objHttpEntity = objHttpResponse.getEntity();
+            objInputStream = objHttpEntity.getContent();
         } catch (Exception e) {
 
             Log.d("Student CheckIn","Error from InputStream ==> " + e.toString());
 
         }
 
+        //change InputStream to string
+        try {
 
+            BufferedReader objBufferedReader = new BufferedReader(new InputStreamReader(objInputStream, "UTF-8"));
+            StringBuilder objStringBuilder = new StringBuilder();
+            String strLine = null;
+            while ((strLine = objBufferedReader.readLine() ) != null) {
+                objStringBuilder.append(strLine);
+            }//while
+
+
+            objInputStream.close();
+            strJSon = objStringBuilder.toString();
+        } catch (Exception e) {
+            Log.d("Student CheckIn", "Error Create String ==>" + e.toString());
+        }
+        //Up Value to SQLite
+        try {
+            final JSONArray objJsonArray = new JSONArray(strJSon);
+            for (int i = 0; i < objJsonArray.length(); i++) {
+                JSONObject objJSONObject = objJsonArray.getJSONObject(i);
+                String strUser = objJSONObject.getString("User");
+                String strPassword = objJSONObject.getString("Password");
+                String strTeacher = objJSONObject.getString("Teacher");
+
+                long insertVale = objUserTABLE.addValueToUser(strUser, strPassword, strTeacher);
+            }//for
+
+        } catch (Exception e) {
+            Log.d("Student CheckIn", "Error Up Value ==>" + e.toString());
+        }
 
     }//synJSonToSQLite
 
